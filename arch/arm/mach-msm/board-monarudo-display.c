87e3707b88c4980c8f066d9250b7b19ffa1ff253
arawas <arawas.jp@gmail.com>
2013-07-21 21:34:55 +0900

    Adjust backlight value to more small.
    And change framerate to 50.

--- arch/arm/mach-msm/board-monarudo-display.c
+++ arch/arm/mach-msm/board-monarudo-display.c
@@ -950,12 +950,12 @@ static void monarudo_display_off(struct msm_fb_data_type *mfd)
 	PR_DISP_INFO("%s\n", __func__);
 }
 
-#define PWM_MIN                   13
-#define PWM_DEFAULT               82
+#define PWM_MIN                   8
+#define PWM_DEFAULT               64
 #define PWM_MAX                   255
 
-#define BRI_SETTING_MIN                 30
-#define BRI_SETTING_DEF                 142
+#define BRI_SETTING_MIN                 8
+#define BRI_SETTING_DEF                 128
 #define BRI_SETTING_MAX                 255
 
 static unsigned char monarudo_shrink_pwm(int val)
@@ -1122,7 +1122,7 @@ static int __init mipi_video_sharp_init(void)
 	pinfo.bpp = 24;
 	pinfo.width = 61;
 	pinfo.height = 110;
-	pinfo.camera_backlight = 176;
+	pinfo.camera_backlight = 1;
 
 	pinfo.lcdc.h_back_porch = 50;
 	pinfo.lcdc.h_front_porch = 100;
@@ -1165,7 +1165,7 @@ static int __init mipi_video_sharp_init(void)
 	pinfo.mipi.stream = 0; 
 	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_SW;
 	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
-	pinfo.mipi.frame_rate = 60;
+	pinfo.mipi.frame_rate = 50;
 	pinfo.mipi.dsi_phy_db = &dsi_video_mode_phy_db;
 	pinfo.mipi.esc_byte_ratio = 6;
 
@@ -1196,7 +1196,7 @@ static int __init mipi_video_sony_init(void)
 	pinfo.bpp = 24;
 	pinfo.width = 61;
 	pinfo.height = 110;
-	pinfo.camera_backlight = 176;
+	pinfo.camera_backlight = 1;
 
 	pinfo.lcdc.h_back_porch = 50;
 	pinfo.lcdc.h_front_porch = 100;
@@ -1239,7 +1239,7 @@ static int __init mipi_video_sony_init(void)
 	pinfo.mipi.stream = 0; 
 	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_SW;
 	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
-	pinfo.mipi.frame_rate = 60;
+	pinfo.mipi.frame_rate = 50;
 	pinfo.mipi.dsi_phy_db = &dsi_video_mode_phy_db;
 
 	ret = mipi_monarudo_device_register(&pinfo, MIPI_DSI_PRIM,
